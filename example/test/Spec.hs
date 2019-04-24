2b78154b0e63db67ca61499df3c5003d69d655bb
Eric Conlon <econlon@lyft.com>
2019-04-23 18:47:10 -0700

    correct

--- example/test/Spec.hs
+++ example/test/Spec.hs
@@ -3,4 +3,7 @@ import Lib (someValue)
 import Control.Monad (unless)
 
 main :: IO ()
-main = unless (someValue == 42) (error "The world has gone mad!")
+main = do
+    putStrLn "Starting test..."
+    unless (someValue == 42) (error "The world has gone mad!")
+    putStrLn "Finished test"
